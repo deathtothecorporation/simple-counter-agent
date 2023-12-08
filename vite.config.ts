10204cc7be45ccb8924d0f4518dd793ebbcb2b9a
coinop-logan <coinop.logan@gmail.com>
2023-12-08 16:49:33 +0200

    point to different urbit address

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:80', // Your Urbit server address
+        target: 'http://127.0.0.1:8080', // Your Urbit server address
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
